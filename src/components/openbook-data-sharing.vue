4374862bb05eab3966efa5aebd4b83db4e99d5e3
Joel Hernandez <lifenautjoe@gmail.com>
2018-04-20 12:22:00 +0200

    🚧(open-book-data-sharing): make shared data items dynamic

--- src/components/openbook-data-sharing.vue
+++ src/components/openbook-data-sharing.vue
@@ -52,10 +52,20 @@
                             <span>your name, profile picture, age range, gender, language, country of birth, friend list, birthday, likes and email address. </span>
                         </div>
                         <div class="column is-12 has-text-centered">
+                            <button class="button is-outlined is-small" @click="goToStep3()">
+                                <span>
+                                    Edit shared data
+                                </span>
+                                <span class="icon">
+                                    <i class="fas fa-pencil-alt"></i>
+                                </span>
+                            </button>
+                        </div>
+                        <div class="column is-12 has-text-centered">
                             <b-collapse :open="false">
                                 <button slot="trigger" class="button is-primary is-outlined is-small">
                                 <span>
-                                    Show data that will be shared
+                                    Show shared data
                                 </span>
                                     <span class="icon">
                                     <i class="fas fa-angle-down"></i>
@@ -63,10 +73,18 @@
                                 </button>
                                 <div class="notification data-container">
                                     <div class="content is-small">
-                                        <p>
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br/>
-                                            Nulla accumsan, metus ultrices eleifend gravida, nulla nunc varius lectus, nec rutrum justo nibh eu lectus. <br/>
-                                            Ut vulputate semper dui. Fusce erat odio, sollicitudin vel erat vel, interdum mattis neque.
+                                        <p v-for="dataItem of sharedData">
+                                            <strong>
+                                                {{ dataItem.name }}:
+                                            </strong>
+                                            <span v-if="dataItem.type === 'text'">
+                                                {{dataItem.value}}
+                                            </span>
+                                            <a v-else-if="dataItem.type === 'list'">
+                                                <span>
+                                                    Open list ↗
+                                                </span>
+                                            </a>
                                         </p>
                                     </div>
                                 </div>
@@ -83,7 +101,7 @@
                                     </button>
                                 </div>
                                 <div class="column is-6">
-                                    <button class="button is-primary is-medium" @click="goToStep3()">
+                                    <button class="button is-primary is-medium" @click="goToStep4()">
                                         Continue
                                     </button>
                                 </div>
@@ -91,7 +109,10 @@
                         </div>
                     </div>
                 </div>
-                <div v-else-if="stepNumber === 3">
+                <div v-else-if="stepNumber === 3" class="third-step">
+
+                </div>
+                <div v-else-if="stepNumber === 4">
                     <div class="columns is-mobile is-multiline">
                         <div class="column is-12 has-text-centered">
                             <span class="icon">
@@ -135,7 +156,7 @@
         border-radius: 50%;
     }
 
-    .section-separator{
+    .section-separator {
         height: 1px;
         width: 100%;
         border-bottom: solid 1px rgba(10, 10, 10, 0.1);
@@ -155,7 +176,118 @@
         name: 'ob-openbook-data-sharing',
         data() {
             return {
-                stepNumber: 2
+                stepNumber: 2,
+                availableData: [
+                    {
+                        readableName: 'name',
+                        name: 'name',
+                        type: 'text',
+                        value: 'Jules Winnfield',
+                        required: true,
+                        shared: true
+                    },
+                    {
+                        readableName: 'age range',
+                        name: 'age_range',
+                        type: 'text',
+                        value: 'Jules Winnfield',
+                        required: false,
+                        shared: true
+                    },
+                    {
+                        readableName: 'gender',
+                        name: 'gender',
+                        type: 'text',
+                        value: 'male',
+                        required: false,
+                        shared: true
+                    },
+                    {
+                        readableName: 'language',
+                        name: 'language',
+                        type: 'text',
+                        value: 'en_US',
+                        required: false,
+                        shared: true
+                    },
+                    {
+                        readableName: 'country of birth',
+                        name: 'birthplace_country',
+                        type: 'text',
+                        value: 'US',
+                        required: false,
+                        shared: true
+                    },
+                    {
+                        readableName: 'friend list',
+                        name: 'friends_list',
+                        type: 'list',
+                        value: [
+                            {
+                                name: 'Vincent Vega',
+                                openBookId: '@vvega'
+                            },
+                            {
+                                name: 'John Doe',
+                                openBookId: '@doejoe'
+                            },
+                            {
+                                name: 'Mike Thompson',
+                                openBookId: '@mikethompsn'
+                            },
+                            {
+                                name: 'Long Johnson',
+                                openBookId: '@ohlongj'
+                            }
+                        ],
+                        required: false,
+                        shared: true
+                    },
+                    {
+                        readableName: 'birthday',
+                        name: 'birthday',
+                        type: 'text',
+                        value: '25-3-1997',
+                        required: false,
+                        shared: true
+                    },
+                    {
+                        readableName: 'likes',
+                        name: 'user_likes',
+                        type: 'list',
+                        value: [
+                            {
+                                name: 'Jack Rabbit Slim\'s Restaurant',
+                                openBookId: '@jackrabbits'
+                            },
+                            {
+                                name: 'Suits & Co.',
+                                openBookId: '@jackrabbits'
+                            },
+                            {
+                                name: 'Crown Pawn Shop',
+                                openBookId: '@crownpawn'
+                            }
+                        ],
+                        required: false,
+                        shared: true
+                    },
+                    {
+                        readableName: 'email address',
+                        name: 'email',
+                        type: 'text',
+                        value: 'badmf@pfiction.com',
+                        required: true,
+                        shared: true
+                    },
+                ]
+            }
+        },
+        computed: {
+            sharedData() {
+                return this.availableData.filter((dataItem) => {
+                    return dataItem.shared;
+                });
             }
         },
         methods: {
@@ -168,7 +300,10 @@
             goToStep3() {
                 this.stepNumber = 3;
             },
-            justAPrototype(){
+            goToStep4() {
+                this.stepNumber = 4;
+            },
+            justAPrototype() {
                 this.$toast.open({
                     message: 'Whoops, this is just a prototype, you\'ll find this info below.',
                     type: 'is-info',
